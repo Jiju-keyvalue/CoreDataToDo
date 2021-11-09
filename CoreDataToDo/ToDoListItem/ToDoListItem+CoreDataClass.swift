ccbba9dbb1e9581448d6dc84f25dd05b7a8874ca
jijusjacob206 <jiju@keyvalue.systems>
2021-11-09 08:28:32 +0530

    completed a to do list in core data

---
+++ CoreDataToDo/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreDataToDo
+//
+//  Created by jiju on 08/11/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
